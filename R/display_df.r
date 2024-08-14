d251091322d364c4150e02a2210e889a3a22a4f9
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-08-14 09:33:06 -0500

    adding caption option

--- R/display_df.r
+++ R/display_df.r
@@ -1,17 +1,18 @@
 #' Displays a formatted table
 #' @param df a data.frame
 #' @param width the width of the columns in pixels
+#' @param caption an optional caption for the table
 #' @examples 
 #'\dontrun{
 #' df <- data.frame(a=c(1,2,3),b=c(4,5,6))
 #' display_df(df)
 #' }
 #' @export
-display_df <- function(df,width=100)
+display_df <- function(df,width=100,caption=NULL)
 {
     width <- paste0("width: ",width,"px")
     styled_df <- htmlTable::addHtmlTableStyle(df,col.rgroup = c("#E7E7E7","#FFFFFF"))
-    IRdisplay::display_html(htmlTable::htmlTable(styled_df, align = "c",
+    IRdisplay::display_html(htmlTable::htmlTable(styled_df, align = "c", caption = caption,
         rnames = FALSE, padding.tspanner = "", ctable = TRUE, css.cell = width, 
         ))
 }
